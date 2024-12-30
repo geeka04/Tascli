0e2afca7152dc56e35b5ce857b7d3890cceac549
Geethika Radhakrishnan <geethikaradhakrishnan@gmail.com>
2024-12-30 15:27:16 +0530

    implemented add-task function

---
+++ task.py
@@ -0,0 +1,53 @@
+import sys
+import json
+import os
+from datetime import datetime
+
+TASK_FILE = "task.json"
+
+# loading the json file
+def load_file():
+    if not os.path.exists(TASK_FILE):
+        return[]
+    with open(TASK_FILE,'r') as file:
+        return json.load(file)
+
+# saving a change to json file
+def save_file(tasks):
+    with open(TASK_FILE,'w') as file:
+        json.dump(tasks, file, indent=2)
+    
+# add a task
+def add_task(description):
+    tasks = load_file()
+    task_id = len(tasks) + 1
+    new_task = {
+        "id" : task_id,
+        "description" : description,
+        "status" : "to-do",
+        "createdAt" :  datetime.now().isoformat(),
+        "updatedAt" : datetime.now().isoformat()
+    }
+    tasks.append(new_task)
+
+    save_file(tasks)
+
+def main():
+    command = sys.argv[1]
+    match command:
+        case "add":
+            if len(sys.argv) < 3:
+                print("Error: Description is required for adding a task.")
+                return
+            description = sys.argv[2]
+            add_task(description)
+
+        case "update":
+            return "one"
+        case "delete":
+            return "two"
+        case default:
+            return "something"
+        
+if __name__ == "__main__":
+    main()
